9b3fef077d92cf64723d5c3a14dfd2bc9c5da806
chax <mcakaric@gmail.com>
2026-02-09 22:54:16 +0100

    eidmiddleware: bump to latest version

--- security/eidmiddleware/actions.py
+++ security/eidmiddleware/actions.py
@@ -8,12 +8,15 @@ NoStrip = ["/usr"]
 IgnoreAutodep = True
 
 Version = get.srcVERSION()
+Release = get.srcRELEASE()
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf eidmiddleware_v%s_amd64.deb" % Version)
-    shelltools.system("tar xf data.tar.xz")
+    shelltools.system("ar xf certiliamiddleware_%s-%s_amd64.deb" % (Version, Release))
+    shelltools.system("zstd -d data.tar.zst")
+    shelltools.system("tar xf data.tar")
 
 def install():
     pisitools.insinto("/", "usr")
     pisitools.insinto("/", "etc")
+    pisitools.insinto("/", "opt")
